cba4ebed2a3066b890942cdfb74e2bdab1539432
Bruno Calixto <bruno.calixto@b2wdigital.com>
2020-05-04 22:59:34 -0300

    add: routers, links, redirect, switch, not found page and prompts

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
+import ManageCoursePage from "./ManageCoursePage";
+import NotFoundPage from "./NotFoundPage";
 import Header from "./common/Header";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route exact path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
